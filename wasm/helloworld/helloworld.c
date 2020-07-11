1b80474575712ffa4a9534a3e701af3789502bd3
gogoend <gogoend@qq.com>
2020-07-11 17:04:43 +0800

    wasm helloworld

---
+++ wasm/helloworld/helloworld.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: gogoend
+ * @Date: 2020-07-11 12:57:27
+ * @LastEditors: gogoend
+ * @LastEditTime: 2020-07-11 16:57:19
+ * @FilePath: \demo\wasm\helloworld\helloworld.c
+ * @Description: 
+ */ 
+
+#include <stdio.h>
+
+int main(){
+  return 0;
+}
+
+int add(int a,int b){
+  return a+b;
+}
