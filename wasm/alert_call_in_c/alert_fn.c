cf4d87fdaf4c9841f735161eee882965c661ba67
gogoend <gogoend@qq.com>
2020-07-11 23:24:14 +0800

    enhance: 在C语言里写alert函数，该函数被调用时会调用浏览器中的alert函数

---
+++ wasm/alert_call_in_c/alert_fn.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: gogoend
+ * @Date: 2020-07-11 12:57:27
+ * @LastEditors: gogoend
+ * @LastEditTime: 2020-07-11 23:20:49
+ * @FilePath: \demo\wasm\alert_call_in_c\alert-fn.c
+ * @Description: 
+ */ 
+#include "alert_fn.h"
+
+void sayHi() {
+	alert("hello,world!");
+}
