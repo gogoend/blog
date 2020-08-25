f401778e17a71f143440d9c7dadec8837759be4f
gogoend <gogoend@qq.com>
2020-08-26 00:53:37 +0800

    MyPromise 0.2 尝试A+标准改造

--- es/promise_from_skectch/my-promise.js
+++ es/promise_from_skectch/my-promise.js
@@ -12,31 +12,39 @@ function MyPromise(func) {
     this.rejectedCbs = []
 
     let _this = this
-    let _resolve = function (val) {
-        let { status, fulfilledCbs } = _this
-        if (status === PENDING) {
-            _this.val = val
-            _this.status = FULFILLED
-            for (let i = 0; i < fulfilledCbs.length; i++) {
-                // 将val作为回调函数的参数返回去
-                fulfilledCbs[i].call(_this,val)
-            }
+    let resolve, _resolve = function (val) {
+        if(val instanceof MyPromise){
+            return value.then(resolve,reject)
         }
+        setTimeout(()=>{
+            let { status, fulfilledCbs } = _this
+            if (status === PENDING) {
+                _this.val = val
+                _this.status = FULFILLED
+                for (let i = 0; i < fulfilledCbs.length; i++) {
+                    // 将val作为回调函数的参数返回去
+                    fulfilledCbs[i].call(_this,val)
+                }
+            }
+        },0)
     }
-    let _reject = function (val) {
-        let { status,rejectedCbs } = _this
-        if (status === PENDING) {
-            _this.val = val
-            _this.status = REJECTED
-            for (let i = 0; i < rejectedCbs.length; i++) {
-                // 将val作为回调函数的参数返回去
-                rejectedCbs[i].call(_this,val)
+    let reject, _reject = function (val) {
+        setTimeout(()=>{
+            let { status,rejectedCbs } = _this
+            if (status === PENDING) {
+                _this.val = val
+                _this.status = REJECTED
+                for (let i = 0; i < rejectedCbs.length; i++) {
+                    // 将val作为回调函数的参数返回去
+                    rejectedCbs[i].call(_this,val)
+                }
             }
-        }
+        },0)
+
     }
 
-    let resolve = _resolve.bind(this)
-    let reject = _reject.bind(this)
+    resolve = _resolve.bind(this)
+    reject = _reject.bind(this)
 
     try{
         func(resolve,reject)
@@ -48,19 +56,9 @@ function MyPromise(func) {
 
 Object.assign(MyPromise.prototype,{
     then:function(onFulfilled,onRejected){
+        let _this = this
         let { status,fulfilledCbs,rejectedCbs,val}=this
         if(status===PENDING){
-            // Promise还在pending的时候，若直接使用：
-            /*
-            fulfilledCbs.push(()=>{
-                onFulfilled(val)
-            })
-            rejectedCbs.push(()=>{
-                onRejected(val)
-            })
-            此时val是从this结构得到的，值为undefined；这样写会导致最后无论如何都会输出undefined。
-            因此，最终调用输出函数时，要重新从this取值，不能使用之前解构的值。
-            */
             fulfilledCbs.push(()=>{
                 onFulfilled(this.val)
             })
