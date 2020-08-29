e1b9ade3c58c02083ef9eb2c539e79c1bac747cf
gogoend <gogoend@qq.com>
2020-08-29 18:03:32 +0800

    MyPromise A+ 代码加测试脚本

--- es/promise_from_skectch/my-promise.js
+++ es/promise_from_skectch/my-promise.js
@@ -4,44 +4,45 @@ const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 
 
-function resolvePromise(promise2,x,resolve,reject){
-    if(promise2 === x){
+function resolvePromise(promise2, x, resolve, reject) {
+    if (promise2 === x) {
         return reject(new TypeError('circular call.'))
     }
     let called = false
-    if(x instanceof MyPromise){
-        if(x.status === PENDING){
-            x.then(y=>{
-                resolvePromise(promise2,y,resolve,reject)
-            },reason=>{
+    if (x instanceof MyPromise) {
+        if (x.status === PENDING) {
+            x.then(y => {
+                resolvePromise(promise2, y, resolve, reject)
+            }, reason => {
                 reject(reason)
             })
         }
-    } else if (x!==null && ((typeof x === 'object')|| typeof x === 'function')){
-        try{
+    } else if (x !== null && ((typeof x === 'object') || typeof x === 'function')) {
+        try {
             let then = x.then
-            if(typeof then ==='function'){
-                then.call(x,y=>{
-                    if(called) return
+            // thenable
+            if (typeof then === 'function') {
+                then.call(x, y => {
+                    if (called) return
                     called = true
-                    resolvePromise(promise2,y,resolve,reject)
-                },reason=>{
-                    if(called) return
+                    resolvePromise(promise2, y, resolve, reject)
+                }, reason => {
+                    if (called) return
                     called = true
                     reject(reason)
                 })
             } else {
                 resolve(x)
             }
-        } catch(err){
-            if(called) return
+        } catch (err) {
+            if (called) return
             called = true
             reject(err)
         }
-    }else{
+    } else {
         resolve(x)
     }
-} 
+}
 
 function MyPromise(func) {
     this.status = PENDING
@@ -53,99 +54,99 @@ function MyPromise(func) {
 
     let _this = this
     let resolve, _resolve = function (val) {
-        if(val instanceof MyPromise){
-            return value.then(resolve,reject)
+        if (val instanceof MyPromise) {
+            return val.then(resolve, reject)
         }
-        setTimeout(()=>{
+        setTimeout(() => {
             let { status, fulfilledCbs } = _this
             if (status === PENDING) {
                 _this.val = val
                 _this.status = FULFILLED
                 for (let i = 0; i < fulfilledCbs.length; i++) {
                     // 将val作为回调函数的参数返回去
-                    fulfilledCbs[i].call(_this,val)
+                    fulfilledCbs[i].call(_this, val)
                 }
             }
-        },0)
+        }, 0)
     }
     let reject, _reject = function (val) {
-        setTimeout(()=>{
-            let { status,rejectedCbs } = _this
+        setTimeout(() => {
+            let { status, rejectedCbs } = _this
             if (status === PENDING) {
                 _this.val = val
                 _this.status = REJECTED
                 for (let i = 0; i < rejectedCbs.length; i++) {
                     // 将val作为回调函数的参数返回去
-                    rejectedCbs[i].call(_this,val)
+                    rejectedCbs[i].call(_this, val)
                 }
             }
-        },0)
+        }, 0)
 
     }
 
     resolve = _resolve.bind(this)
     reject = _reject.bind(this)
 
-    try{
-        func(resolve,reject)
-    } catch(err) {
+    try {
+        func(resolve, reject)
+    } catch (err) {
         reject(err)
     }
 
 }
 
-Object.assign(MyPromise.prototype,{
-    then:function(onFulfilled,onRejected){
+Object.assign(MyPromise.prototype, {
+    then: function (onFulfilled, onRejected) {
         let _this = this
-        let { status,fulfilledCbs,rejectedCbs}=this
+        let { status, fulfilledCbs, rejectedCbs } = this
 
         onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
-        onRejected = typeof onRejected === 'function' ? onRejected : err => {throw(err)}
+        onRejected = typeof onRejected === 'function' ? onRejected : err => { throw (err) }
 
-        let promise2 = new MyPromise((resolve,reject)=>{
-            if(status===PENDING){
-                fulfilledCbs.push((val)=>{
-                    setTimeout(()=>{
-                        try{
+        let promise2 = new MyPromise((resolve, reject) => {
+            if (status === PENDING) {
+                fulfilledCbs.push((val) => {
+                    setTimeout(() => {
+                        try {
                             let x = onFulfilled(val)
-                            resolvePromise(newPromise, x, resolve, reject)
-                        }catch(err){
+                            resolvePromise(promise2, x, resolve, reject)
+                        } catch (err) {
                             reject(err)
                         }
-                    },0)
+                    }, 0)
                 })
-                rejectedCbs.push((val)=>{
-                    setTimeout(()=>{
-                        try{
+                rejectedCbs.push((val) => {
+                    setTimeout(() => {
+                        try {
                             let x = onRejected(val)
-                            resolvePromise(newPromise, x, resolve, reject)
-                        }catch(err){
+                            resolvePromise(promise2, x, resolve, reject)
+                        } catch (err) {
                             reject(err)
                         }
-                    },0)
+                    }, 0)
                 })
             }
 
-            if(status===FULFILLED){
-                setTimeout((val)=>{
+            if (status === FULFILLED) {
+                setTimeout((val) => {
                     try {
                         let x = onFulfilled(val)
-                        resolvePromise(newPromise, x, resolve, reject)
-                    }catch(err){
+                        resolvePromise(promise2, x, resolve, reject)
+                    } catch (err) {
                         reject(err)
                     }
-                },0)
+                }, 0)
             }
 
-            if(status===REJECTED){
-                setTimeout((val)=>{
+            if (status === REJECTED) {
+                setTimeout((val) => {
                     try {
                         let x = onRejected(val)
-                        resolvePromise(newPromise, x, resolve, reject)
-                    }catch(err){
+                        resolvePromise(promise2, x, resolve, reject)
+                    } catch (err) {
                         reject(err)
                     }
-                },0)
+                }, 0)
             }
         })
 
@@ -153,5 +154,18 @@ Object.assign(MyPromise.prototype,{
 
     }
 })
+
+// // --------- 测试用 ---------
+// MyPromise.deferred = function () { // 延迟对象
+//     let defer = {};
+//     defer.promise = new MyPromise((resolve, reject) => {
+//         defer.resolve = resolve;
+//         defer.reject = reject;
+//     });
+//     return defer;
+// }
+
+// try { module.exports = MyPromise } catch (e) {}
+// // -------------------------
 
 export default MyPromise
