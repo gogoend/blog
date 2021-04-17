2d03e820cdbbe9f74ec33fdb2f1b49397214e2b7
gogoend <gogoend@qq.com>
2021-04-17 13:00:22 +0800

    更新: 完善开始点、结束点拖动功能，同时连线正常

--- svg/link-node-dragger/pointer-helper.js
+++ svg/link-node-dragger/pointer-helper.js
@@ -1,37 +1,39 @@
 function pointerHelper(ev, handlers) {
   const start = handlers.start || (() => void 0)
-  const moveInner = handlers.moveInner || (() => void 0)
-  const moveOuter = handlers.moveOuter || (() => void 0)
+  // const moveInner = handlers.moveInner || (() => void 0)
+  // const moveOuter = handlers.moveOuter || (() => void 0)
   const move = handlers.move || (() => void 0)
   const end = handlers.end || (() => void 0)
 
   let _this = this
-  const startEl = ev.target
+  const originEl = ev.target
 
   start.call(_this, ev)
   const handleTargetTouchEvent = (ev) => {
     switch (ev.type) {
       case 'mousemove': {
-        const touch = ev // .changedTouches[0]
-        const touchingEl = document.elementFromPoint(touch.pageX, touch.pageY - document.documentElement.scrollTop)
-        if (touchingEl === startEl) {
-          moveInner.call(_this, ev)
-        } else {
-          moveOuter.call(_this, ev)
-        }
-        move.call(_this, ev)
+        // const touch = ev // .changedTouches[0]
+        // const touchingEl = document.elementFromPoint(touch.pageX, touch.pageY - document.documentElement.scrollTop)
+        // if (touchingEl === startEl) {
+        //   moveInner.call(_this, ev)
+        // } else {
+        //   moveOuter.call(_this, ev)
+        // }
+        move.call(_this, ev, {
+          originEl
+        })
         break;
       }
-      case 'mouseup':
-      case 'mouseout': {
+      case 'mouseup':{
         document.removeEventListener('mousemove', handleTargetTouchEvent)
-        document.removeEventListener('mouseout', handleTargetTouchEvent)
-        end.call(_this, ev)
+        document.addEventListener('mouseup', handleTargetTouchEvent)
+        end.call(_this, ev, {
+          originEl
+        })
         break;
       }
     }
   }
   document.addEventListener('mousemove', handleTargetTouchEvent)
-  document.addEventListener('mouseout', handleTargetTouchEvent)
   document.addEventListener('mouseup', handleTargetTouchEvent)
 }
