d773b59c629545325a1227112d97080804fb831d
gogoend <gogoend@qq.com>
2021-09-02 23:45:11 +0800

    feat(axios源码): 实现拦截器各项的依次、正确执行（借组Promise）

--- network/axios/202109022125-simple-source-code-from-netease-class/index.js
+++ network/axios/202109022125-simple-source-code-from-netease-class/index.js
@@ -26,6 +26,7 @@ export function Axios () {
 Axios.prototype.request = function () {
     // 1. 组成数组 - 初始数组第一个元素为真正的发请求的
     let chain = [dispatchRequest, undefined]
+    let promise = Promise.resolve()
     this.interceptor.request.handler.forEach((interceptor) => {
         // 成对插入
         chain.unshift(interceptor.fulfilled, interceptor.rejected)
@@ -36,9 +37,8 @@ Axios.prototype.request = function () {
     })
 
     // 2. 执行数组里的，每一个方法
-    // !!! 注意不是这样的 
-    // 首先这样做会导致无论是同步函数还是异步函数都会被立即执行 - 特别是夹在中间的请求函数，请求未执行完成就就将直接执行后方的响应拦截器
-    // 另外这里成功/失败回调是相间执行的，因此所有情况下的回调都会被执行 - 显然不太好
-    // chain.forEach(fn => { fn && fn() })
-    
+    while(chain.length){
+        promise = promise.then(chain.shift(), chain.shift())
+    }
+    return promise
 }
