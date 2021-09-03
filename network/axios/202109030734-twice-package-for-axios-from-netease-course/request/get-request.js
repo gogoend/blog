770a832985b81e916988c01f738c78999cb2632f
gogoend <gogoend@qq.com>
2021-09-03 08:26:36 +0800

    feat(axios二次封装): 大致请求自动绑定 - 直接在请求函数中为组件上的对应字段赋值

--- network/axios/202109030734-twice-package-for-axios-from-netease-course/request/get-request.js
+++ network/axios/202109030734-twice-package-for-axios-from-netease-course/request/get-request.js
@@ -2,6 +2,7 @@ import server from './server.js'
 
 function myServer () {
     this.server = server
+    this.nowHandle = null
 }
 
 myServer.prototype.parseRouter = function (name, urlOb) {
@@ -11,15 +12,23 @@ myServer.prototype.parseRouter = function (name, urlOb) {
     })
 }
 
+myServer.prototype.v = function (vueObj) {
+    this.nowHandle = vueObj
+}
+
 // 轮子要留出扩展接口
 myServer.prototype.send = function (moduleName,name, url, config) {
     const _this = this
     let config = config || {}
     let type = config.type || 'get' // 应该写在具体业务中
     let data = config.data || {}
+
+    let bindName = config.bindName || name
+
     // 对于请求响应的处理分为两个模块
     // 效果处理模块，例如加loading
     let before = function (res) {
+        _this.nowHandle[bindName] = res.data
         return res
     }
     let defaultFn = function (res) {
