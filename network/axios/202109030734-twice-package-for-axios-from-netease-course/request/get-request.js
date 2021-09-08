04e8da87ee81ac38f3d7b0d2682ca47badb679fb
gogoend <gogoend@qq.com>
2021-09-09 01:10:24 +0800

    feat(axios二次封装): 编写axios防止重复请求的处理逻辑 - 核心是在每个接口请求函数上加status静态属性

--- network/axios/202109030734-twice-package-for-axios-from-netease-course/request/get-request.js
+++ network/axios/202109030734-twice-package-for-axios-from-netease-course/request/get-request.js
@@ -9,11 +9,13 @@ myServer.prototype.parseRouter = function (name, urlOb) {
     this[name] = {}
     Object.keys(urlOb).forEach(apiName => {
         this[name][apiName] = this.sendMes.bind(this, name, apiName, urlOb[apiName])
+        this[name][apiName].state = 'ready'
     })
 }
 
 myServer.prototype.v = function (vueObj) {
     this.nowHandle = vueObj
+    return vueObj
 }
 
 // 轮子要留出扩展接口
@@ -28,13 +30,18 @@ myServer.prototype.send = function (moduleName,name, url, config) {
     // 对于请求响应的处理分为两个模块
     // 效果处理模块，例如加loading
     let before = function (res) {
-        _this.nowHandle[bindName] = res.data
+        self[moduleName][name].state = 'ready'
         return res
     }
     let defaultFn = function (res) {
+        _this.nowHandle[bindName] = res.data
         return res
     }
     let success = config.success || defaultFn
-    this.server[type](url, data).then(before).then(success)
+
+    if(this[moduleName][name].state === 'ready') {
+        this.server[type](url, data).then(before).then(success)
+        this[moduleName][name].state = 'wating'
+    }
 }
 export default new myServer()
