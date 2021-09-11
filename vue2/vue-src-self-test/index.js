5a3756b6f1c91b2fab056966a6c3e37e4ffa5357
gogoend <gogoend@qq.com>
2021-09-11 18:47:47 +0800

    refactor(vue-src-self-test): observe 中递归添加响应式的逻辑移入defineObserve中进行调用；加入注释进行补充

--- vue2/vue-src-self-test/index.js
+++ vue2/vue-src-self-test/index.js
@@ -3,32 +3,37 @@ function isObject (object) {
 }
 
 function defineObserve(object, key) {
+    // 当前的属性值为
     let value = object[key]
+    // 当前的属性值对应的依赖管理器 - 依赖管理器包含有一些依赖该值的watcher
     let dep = {
         notify() {
             console.log(`${key}触发了变化，去通知依赖我的Watcher进行更新`)
         }
     }
-
+    // 为当前属性值添加响应式
+    observe(value)
     Object.defineProperty(object, key, {
         set (val) {
             value = val
             dep.notify()
-            observe(val)
+            // 为新的属性值添加响应式
+            observe(value)
         },
         get () {
             return value
         },
-        enumerable: true
+        enumerable: true,
+        configurable: true
     })
 }
 
+// 响应式入口
 export function observe (object) {
     if (!isObject(object)) return
     for(let key in object) {
         if (Object.prototype.hasOwnProperty.call(object, key)) {
             defineObserve(object, key)
-            observe(object[key])
         }
     }
 }
