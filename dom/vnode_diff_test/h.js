5e5443c4b06150b2dcb1a682a24fd5ffd73ec8db
gogoend <gogoend@qq.com>
2021-11-15 00:27:43 +0800

    perf(vnode_dif_test h): 增强children参数判断，防止该参数未传入时，导致下方`.map`操作报错；判断了该参数可能为number的情况

--- dom/vnode_diff_test/h.js
+++ dom/vnode_diff_test/h.js
@@ -1,11 +1,14 @@
 import vnode from './vnode.js'
 
 export default function h(type, data, children) {
-  if (typeof children === "string") {
+  if ([
+    "string",
+    "number"
+  ].includes(typeof children)) {
     children = [children];
   }
   // 判断子节点是否为字符串；如果是也把它转成vnode
-  children = children.map((child) => {
+  children = (children || []).map((child) => {
     if (typeof child === "string") {
       return vnode(undefined, undefined, undefined, child, undefined);
     } else {
