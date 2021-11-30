bf465ccd19c767d1da5abfc78756c938dc9f3285
gogoend <gogoend@qq.com>
2021-12-01 00:58:51 +0800

    feat(recorder): 加入`createAnalysisNode`；简单完善节点布局逻辑

--- audiocontext/recorder/recorder.js
+++ audiocontext/recorder/recorder.js
@@ -3,40 +3,83 @@ function createCollectNode(audioContext) {
     const BUFFER_SIZE = 4096;
     const INPUT_CHANNEL_COUNT = 2;
     const OUTPUT_CHANNEL_COUNT = 2;
-    let creator = audioContext.createScriptProcessor;
-    creator = creator.bind(audioContext);
-    return creator(BUFFER_SIZE, INPUT_CHANNEL_COUNT, OUTPUT_CHANNEL_COUNT);
+    let creator = function () {
+        let node = audioContext.createScriptProcessor(BUFFER_SIZE, INPUT_CHANNEL_COUNT, OUTPUT_CHANNEL_COUNT);
+        return node;
+    };
+    return creator();
+}
+function createAnalysisNode(audioContext) {
+    const FFT_SIZE = 32;
+    let creator = function () {
+        let node = audioContext.createAnalyser();
+        node.fftSize = FFT_SIZE;
+        return node;
+    };
+    return creator();
 }
 function onAudioProcess(ev, lChannelList) {
-    let audioBuffer = ev.inputBuffer;
-    let leftChannelData = audioBuffer.getChannelData(0);
-    lChannelList.push(leftChannelData.slice(0));
+    let inputBuffer = ev.inputBuffer;
+    let inputLeftChannelData = inputBuffer.getChannelData(0);
+    lChannelList.push(inputLeftChannelData.slice(0));
+    // // outputBuffer内容默认全为0，除非类似此处手动将inputBuffer赋值给outputBuffer
+    // let outputBuffer = ev.outputBuffer
+    // let outputLeftChannelData = outputBuffer.getChannelData(0)
+    // // Loop through the 4096 samples
+    // for (let sample = 0; sample < inputBuffer.length; sample++) {
+    //     // make output equal to the same as the input
+    //     outputLeftChannelData[sample] = inputLeftChannelData[sample];
+    //     // add noise to each output sample
+    //     outputLeftChannelData[sample] += ((Math.random() * 2) - 1) * 0.2;
+    // }
 }
 export default class Recorder {
     leftChannelList = [];
     aCtx = null;
     mediaStream = null;
+    // 三个节点
     collectNode = null;
-    audioContextNodes = [];
+    analysisNode = null;
+    mediaStreamNode = null;
+    // 节点连线集合
+    audioContextPaths = [];
     constructor(mediaStream) {
         this.mediaStream = mediaStream;
         this.aCtx = new AudioContext;
+        // 三个节点初始化赋值
+        this.mediaStreamNode = this.aCtx.createMediaStreamSource(mediaStream);
         this.collectNode = createCollectNode(this.aCtx);
-        this.audioContextNodes = [
-            this.aCtx.createMediaStreamSource(mediaStream),
+        this.analysisNode = createAnalysisNode(this.aCtx);
+        // 布局节点路径
+        this.audioContextPaths.push([
+            this.mediaStreamNode,
             this.collectNode,
             this.aCtx.destination
-        ];
+        ]);
+        this.audioContextPaths.push([
+            this.mediaStreamNode,
+            this.analysisNode
+        ]);
+        // 连接节点
         this.linkNodes();
+        // // 音频分析
+        // var frequencyBinCount = this.analysisNode.frequencyBinCount;
+        // var dataArray = new Uint8Array(frequencyBinCount);
+        // setInterval(() => {
+        //     this.analysisNode.getByteFrequencyData(dataArray)
+        //     console.log(dataArray)
+        // }, 500)
     }
     linkNodes() {
-        for (let i = 0; i < this.audioContextNodes.length; i++) {
-            let currentNode = this.audioContextNodes[i];
-            let nextNode = this.audioContextNodes[i + 1];
-            if (nextNode) {
-                currentNode.connect(nextNode);
+        this.audioContextPaths.forEach(path => {
+            for (let i = 0; i < path.length; i++) {
+                let currentNode = path[i];
+                let nextNode = path[i + 1];
+                if (nextNode) {
+                    currentNode.connect(nextNode);
+                }
             }
-        }
+        });
     }
     beginRecord() {
         this.collectNode.onaudioprocess = (ev) => {
@@ -48,7 +91,9 @@ export default class Recorder {
     }
     destory() {
         this.mediaStream.getAudioTracks()[0].stop();
-        this.audioContextNodes.forEach(node => node.disconnect());
+        this.audioContextPaths.forEach(path => {
+            path.forEach(node => node.disconnect());
+        });
     }
     saveWaveFile() {
         let leftData = mergeArray(this.leftChannelList);
