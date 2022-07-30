3b5ad1c30cff096dc16b468f8f79e937b7b99da2
gogoend <gogoend@qq.com>
2022-07-30 15:01:42 +0800

    refactor(word-statistics): 代码结构微调；加入待爬取页面列表

--- nodejs/puppeteer/word-statistics/index.js
+++ nodejs/puppeteer/word-statistics/index.js
@@ -3,9 +3,11 @@ const cleanIgnoredElements = require('./clean-ignored-elements')
 const cleanDuplicatedSpaces = require('./clean-duplicated-spaces')
 
 const pages = []
-async function processPage(page) {
-  pages.push(page)
+async function processPage(browser) {
+  let page = await browser.newPage();
   await page.goto('http://localhost:8080/docs/manual/en/introduction/Creating-a-scene.html')
+  pages.push(page)
+
   await new Promise(resolve => setTimeout(resolve, 2000))
 
   // 在浏览器端注入函数 - 使用Function#toString将函数体转换为字符串
@@ -16,20 +18,22 @@ async function processPage(page) {
 
   let a = await page.$eval('body', async (el) => {
     await window.cleanIgnoredElements(el)
-
-    debugger
     return await window.cleanDuplicatedSpaces(el.innerText)
   })
 
   console.log(a.split(/\s/).length)
+  await page.close()
+
+  let index = pages.findIndex((it) => it === page)
+  if (index > -1) {
+    pages.splice(index, 1)
+  }
 }
 
 ;(async () => {
   const browser = await puppeteer.launch({ headless: false })
 
-  let page = await browser.newPage();
-
-  await processPage(page)
+  await processPage(browser)
 
   ;await new Promise(() => {})
   ;await browser.close()
---
+++ nodejs/puppeteer/word-statistics/page-url-list.js
@@ -0,0 +1,21 @@
+const base = `http://localhost:8080`
+
+module.exports = [
+  "/docs/manual/en/introduction/Animation-system.html",
+  "/docs/manual/en/introduction/Browser-support.html",
+  "/docs/manual/en/introduction/Creating-a-scene.html",
+  "/docs/manual/en/introduction/Creating-text.html",
+  "/docs/manual/en/introduction/Drawing-lines.html",
+  "/docs/manual/en/introduction/FAQ.html",
+  "/docs/manual/en/introduction/How-to-create-VR-content.html",
+  "/docs/manual/en/introduction/How-to-dispose-of-objects.html",
+  "/docs/manual/en/introduction/How-to-run-things-locally.html",
+  "/docs/manual/en/introduction/How-to-update-things.html",
+  "/docs/manual/en/introduction/How-to-use-post-processing.html",
+  "/docs/manual/en/introduction/Installation.html",
+  "/docs/manual/en/introduction/Loading-3D-models.html",
+  "/docs/manual/en/introduction/Matrix-transformations.html",
+  "/docs/manual/en/introduction/Typescript-setup.html",
+  "/docs/manual/en/introduction/Useful-links.html",
+  "/docs/manual/en/introduction/WebGL-compatibility-check.html"
+]
