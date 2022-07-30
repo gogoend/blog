00225aebb5aaa07f67f2664c7bbd72e7df974848
gogoend <gogoend@qq.com>
2022-07-30 18:05:58 +0800

    feat(word-statistics): 调整统计数据在打印、存储时的数据结构

--- nodejs/puppeteer/word-statistics/index.js
+++ nodejs/puppeteer/word-statistics/index.js
@@ -5,12 +5,13 @@ const cleanDuplicatedSpaces = require('./clean-duplicated-spaces')
 const urlList = require('./page-url-list').list
 
 const pages = []
+const charCountInPages = []
 async function processPage(browser, url) {
-  let page = await browser.newPage();
+  let page = await browser.newPage()
   await page.goto(url)
   pages.push(page)
 
-  await new Promise(resolve => setTimeout(resolve, 2000))
+  await new Promise(resolve => setTimeout(resolve, 2500))
 
   // 在浏览器端注入函数 - 使用Function#toString将函数体转换为字符串
   await page.addScriptTag({
@@ -23,7 +24,14 @@ async function processPage(browser, url) {
     return await window.cleanDuplicatedSpaces(el.innerText)
   })
 
-  console.log(a.split(/\s/).length)
+  const currentPageCount = {
+    url,
+    count: a.split(/\s/).length
+  }
+  console.log(currentPageCount)
+  charCountInPages.push(
+    currentPageCount
+  )
   await page.close()
 
   let index = pages.findIndex((it) => it === page)
@@ -33,7 +41,7 @@ async function processPage(browser, url) {
 }
 
 ;(async () => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
 
   // const promiseFactory = urlList.map(url => {
     // return () => processPage(
@@ -43,7 +51,7 @@ async function processPage(browser, url) {
   // })
 
   let cursor = 0
-  const CONCURRENCY = 20
+  const CONCURRENCY = 30
   const originLength = urlList.length
 
   while(cursor < originLength) {
@@ -59,6 +67,9 @@ async function processPage(browser, url) {
       )
     )
   }
+  console.log(
+    JSON.stringify(charCountInPages)
+  )
 
   ;await new Promise(() => {})
   ;await browser.close()
