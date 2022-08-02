cec765569e443283545c65bca0d4be8dfc30028a
gogoend <gogoend@qq.com>
2022-08-02 08:39:37 +0800

    feat(obj-deep-clone-v2): 加入memoMap用以建立记录拷贝过的对象到新对象之间的映射关系，防止出现环

--- es/obj_deep_clone-v2/index.js
+++ es/obj_deep_clone-v2/index.js
@@ -8,15 +8,21 @@ let isBasicValue = val => val === null || ['string', 'boolean', 'number', 'bigin
 
 let hasOwn = (object, key) => Object.prototype.hasOwnProperty.call(object, key)
 
-let cloneDeep = val => {
+let cloneDeep = (val, memoMap = new Map()) => {
     if(isBasicValue(val)){
         return val
     }
+    if(memoMap.has(val)) {
+        return memoMap.get(val)
+    }
+
     if (getObjectTagFromValue(val) === concatObjectTag('Object')) {
         let newVal = new val.constructor()
+        memoMap.set(val, newVal)
+
         for(let key in val) {
             if (hasOwn(val, key)) {
-                newVal[key] = cloneDeep(val[key])
+                newVal[key] = cloneDeep(val[key], memoMap)
             }
         }
 
@@ -24,18 +30,23 @@ let cloneDeep = val => {
     }
     if (getObjectTagFromValue(val) === concatObjectTag('Array')) {
         let newVal = new val.constructor()
+        memoMap.set(val, newVal)
+
         for (let i = 0; i < val.length; i++) {
             if (hasOwn(val, i)){
-                newVal[i] = cloneDeep(val[i])
+                newVal[i] = cloneDeep(val[i], memoMap)
             }
         }
         
         return newVal
     }
+
+    return val
 }
 
 // 3_gogoendA
 gogoend_a = [{b:{c: {d: { e: { f: [1,2,3, {g: {h: {}}}]} }}}},{i:{j: {k: { l: { m: [4,5,6, {n: {o: {}}}]} }}}}]
+gogoend_a.push(gogoend_a)
 
 // 4_gogoendB
 gogoend_b = cloneDeep(gogoend_a)
