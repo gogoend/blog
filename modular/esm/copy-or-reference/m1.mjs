f0ad2a9c80bd2193f858e85c5b81f47f76d571df
gogoend <gogoend@qq.com>
2022-08-07 14:49:16 +0800

    feat(模块化 esm): 了解值esm导入导出使用的是引用还是拷贝？

---
+++ modular/esm/copy-or-reference/m1.mjs
@@ -0,0 +1,13 @@
+export const people = [{
+    name: 'Bob',
+    phone: '10086',
+    age: 19
+}, {
+    name: 'Amy',
+    phone: '10001',
+    age: 28
+}]
+
+export let limitAge = 29
+export const addLimitAge = () => { limitAge++ }
+
