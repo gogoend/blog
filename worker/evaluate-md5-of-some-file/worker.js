2dbbbabd41137c46e9c28a67bd9ee3ac9fa40e0d
gogoend <gogoend@qq.com>
2022-08-23 04:19:14 +0800

    feat(worker 计算文件md5): 加入基本的MD5计算逻辑，初步实现单个文件的MD5计算

--- worker/evaluate-md5-of-some-file/worker.js
+++ worker/evaluate-md5-of-some-file/worker.js
@@ -1,19 +1,34 @@
 importScripts('./hash-wasm-md5.umd.min.js');
 
-async function run() {
-  const { md5 } = hashwasm
+async function normalizeInput (input) {
+  if (input instanceof Blob) {
+    return new Uint8Array(await input.arrayBuffer())
+  }
+}
 
-  console.log('MD5:', await md5('demo'));
+const commandMap = {
+  async computeMd5SumOfFile (input) {
+    const { md5 } = hashwasm
+    // console.log(
+    //   new Uint8Array(await file.arrayBuffer())
+    // )
+
+    return await md5(
+      await normalizeInput(
+        input
+      )
+    )
+  }
 }
 
 self.addEventListener(
   'message',
-  (ev) => {
+  async (ev) => {
     const { data, command } = ev.data ?? {}
-    if (command === 'file') {
-      if (data) {
-        self.postMessage('777')
-      }
+
+    if (typeof commandMap[command] === 'function') {
+      const result = await commandMap[command](data)
+      self.postMessage(result)
     }
   }
 )
