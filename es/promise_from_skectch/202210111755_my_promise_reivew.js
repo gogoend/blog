2652e9f405ac34bd5d5658cfea2f19aaee9f6fd1
gogoend <gogoend@qq.com>
2022-10-11 23:44:41 +0800

    feat(es promise): 完善异步调用；完善then参数；完善catch执行

--- es/promise_from_skectch/202210111755_my_promise_reivew.js
+++ es/promise_from_skectch/202210111755_my_promise_reivew.js
@@ -1,60 +1,67 @@
 class MyPromise {
     result;
     reason;
-    status = 'PENDING';
+    status = "PENDING";
     constructor(fn) {
         const resolve = (result)=>{
-            setTimeout(()=>{
-                if (this.status === 'PENDING') {
-                    this.status = 'FULFILLED'
-                    this.result = result // resolveResult(result)
-                    this.fulfillCbs.forEach(cb=>{
-                        cb(result)
-                    }
-                    )
+            if (this.status === "PENDING") {
+                this.status = "FULFILLED";
+                this.result = result;
+                // resolveResult(result)
+                this.fulfillCbs.forEach((cb)=>{
+                    cb(result);
                 }
+                );
             }
-            )
         }
+        ;
         const reject = (reason)=>{
-            setTimeout(()=>{
-                if (this.status === 'PENDING') {
-                    this.status = 'REJECTED'
-                    this.reason = reason
-                    this.rejectCbs.forEach(cb=>{
-                        cb(reason)
-                    }
-                    )
+            if (this.status === "PENDING") {
+                this.status = "REJECTED";
+                this.reason = reason;
+                this.rejectCbs.forEach((cb)=>{
+                    cb(reason);
                 }
+                );
             }
-            )
         }
-        fn(resolve, reject)
+        ;
+        try {
+            fn(resolve, reject);
+        } catch (reason) {
+            reject(reason);
+        }
     }
 
     fulfillCbs = [];
     rejectCbs = [];
-    then(fn) {
-        if (this.status === 'FULFILLED') {
-            fn(this.result)
+    then(successFn, failFn) {
+        if (this.status === "FULFILLED") {
+            setTimeout(successFn, 0, this.result);
         } else {
-            this.fulfillCbs.push(fn)
+            this.fulfillCbs.push(()=>setTimeout(successFn, 0, this.result));
         }
 
+        if (failFn) {
+            this.catch(failFn);
+        }
     }
-    ;catch(fn) {
-        if (this.status === 'REJECTED') {
-            fn(this.reason)
+    catch(fn) {
+        if (this.status === "REJECTED") {
+            setTimeout(fn, 0, this.reason);
         } else {
-            this.rejectCbs.push(fn)
+            this.rejectCbs.push(()=>setTimeout(fn, 0, this.reason));
         }
     }
 }
 
 z = new MyPromise((resolve,reject)=>{
-    resolve(1)
+    reject(1);
 }
-).then(res=>{
-    console.log(res)
+).then((res)=>{
+    console.log(res);
 }
-)
+, (reason)=>{
+    console.error(reason);
+}
+);
