d8df7f8a75363be6943ab1f22bf320e211bc5c03
gogoend <gogoend@qq.com>
2023-04-06 01:56:47 +0800

    feat(vue-src-relative-4): 加入Dep类用于存储每个属性所对应的watcher；加入watch方法用于监听对象中数据变化

--- vue2/vue-src-relative-4/index.js
+++ vue2/vue-src-relative-4/index.js
@@ -1,20 +1,64 @@
-export const observe = (o) => {
-  Object.keys(o).forEach(key => {
-    let val = o[key]
-    if (o[key] && typeof o[key] === 'object') {
-      observe(o[key])
+class Dep {
+  subs = new Array()
+  subSet = new Set()
+  addSub (fn) {
+    if (!this.subSet.has(fn)) {
+      this.subs.push(fn)
+      this.subSet.add(fn)
     }
+  }
+  removeSub (fn) {
+    const indexOfFn = this.subs.indexOf(fn)
+    if (indexOfFn >= 0) {
+      this.subs.splice(
+        indexOfFn, 1
+      )
+    }
+    this.subSet.remove(fn)
+  }
+  notify () {
+    this.subs.forEach(fn => fn())
+  }
 
-    Object.defineProperty(o, key, {
-      get () {
-        return val
-      },
-      set (v) {
-        val = v
-        if (v && typeof v === 'object') {
-          observe(v)
-        }
+  /**
+   * @type {Function | null}
+   */
+  static depTarget = null
+}
+
+export function watch(expressionToExecute, cb) {
+  Dep.depTarget = cb
+  expressionToExecute()
+  Dep.depTarget = null
+}
+
+function defineReactive (o, key) {
+  let val = o[key]
+  if (o[key] && typeof o[key] === 'object') {
+    observe(o[key])
+  }
+  const dep = new Dep()
+
+  Object.defineProperty(o, key, {
+    get () {
+      if (Dep.depTarget) {
+        dep.addSub(Dep.depTarget)
       }
-    })
+      return val
+    },
+    set (v) {
+      val = v
+      observe(v)
+      dep.notify()
+    }
+  })
+}
+
+export const observe = (o) => {
+  if (!o || typeof o !== 'object') {
+    return
+  }
+  Object.keys(o).forEach(key => {
+    defineReactive(o, key)
   })
 }
