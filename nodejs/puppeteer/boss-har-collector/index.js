ef2daecb2d8176a6a2dd5a4e44534a522f08e4fb
gogoend <gogoend@qq.com>
2023-12-28 12:16:35 +0800

    feat(node puppeteer boss-har-collector): 完善position之间自动切换的逻辑；去掉存储为har的逻辑-存不了响应体

--- nodejs/puppeteer/boss-har-collector/index.js
+++ nodejs/puppeteer/boss-har-collector/index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 const cookies = require('./cookies')
-const PuppeteerHar = require('puppeteer-har');
 
 if (!cookies?.length) {
   console.error('There is no cookies. you can save a copy with EditThisCookie extension.')
@@ -12,20 +11,6 @@ const asyncPoll = require('tiny-async-pool')
 const url = `https://www.zhipin.com/web/chat/index`
 const pages = []
 globalThis.pages = pages
-async function processPage(browser, url) {
-  let page = await browser.newPage()
-
-  //设置cookie
-  for(let i = 0; i < cookies.length; i++){
-    await page.setCookie(cookies[i]);
-  }
-
-  await page.goto(url, { timeout: 0 })
-  pages.push(page)
-
-  await new Promise(resolve => setTimeout(resolve, 2500))
-  return page
-}
 
 function sleep (t) {
   return new Promise(resolve => {
@@ -38,66 +23,98 @@ function sleepWithRandomDelay (base) {
 }
 
 ;(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    ignoreHTTPSErrors: true,
-    defaultViewport: {
-      width: 1440,
-      height: 900,
-    },
-    devtools: true
-  })
-
-  const page = await processPage(browser, url)
-
-  const recommendGeekLink = (await pages[0].$('a[ka=menu-geek-recommend]'))
-  await recommendGeekLink.click()
-
-  const har = new PuppeteerHar(page);
-  await har.start({ path: 'results.har' });
-  await sleepWithRandomDelay(3000)
-
-  const recommendIframe = (await pages[0].$('iframe[name=recommendFrame]'))
-  const recommendIframeBBox = await recommendIframe.boundingBox()
-  await pages[0].mouse.move(
-    recommendIframeBBox.x + recommendIframeBBox.width / 2,
-    recommendIframeBBox.y + recommendIframeBBox.height / 2
-  )
-
-  await pages[0].mouse.wheel({deltaY: 2000});
-  await sleepWithRandomDelay(1000)
-  await pages[0].mouse.wheel({deltaY: 2000});
-  await sleepWithRandomDelay(1000)
-  await pages[0].mouse.wheel({deltaY: 2000});
-  await sleepWithRandomDelay(1000)
-  await pages[0].mouse.wheel({deltaY: 2000});
-  await sleepWithRandomDelay(1000)
-  await pages[0].mouse.wheel({deltaY: 2000});
-  await sleepWithRandomDelay(1000)
-  await pages[0].mouse.wheel({deltaY: -10000});
-
-  const recommendIframeForControl = (await pages[0].frames()).find(it => it._name === 'recommendFrame')
-  const firstGeekCardItemClickArea = await recommendIframeForControl.$(`ul.card-list li.card-item:first-child .card-inner.common-wrap`)
-  await sleepWithRandomDelay(1000)
-
-  await firstGeekCardItemClickArea.click()
-  for (let i = 1; i <= 45; i++) {
-    await sleepWithRandomDelay(1000)
-
-    const highlightItems = (await recommendIframeForControl.$$(`.resume-item.item-base .font-hightlight`)) ?? []
-    for(let j =0; j < highlightItems.length; j++) {
-      if (Math.random() > 0.5) {
-        continue
-      }
-      const it = highlightItems[j]
-      await sleepWithRandomDelay(400)
-      await it.click()
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+      ignoreHTTPSErrors: true,
+      defaultViewport: {
+        width: 1440,
+        height: 900,
+      },
+      devtools: true
+    })
+  
+    const page = await browser.newPage()
+  
+    //设置cookie
+    for(let i = 0; i < cookies.length; i++){
+      await page.setCookie(cookies[i]);
     }
+  
+    await page.goto(url, { timeout: 0 })
+    pages.push(page)
+  
+    await new Promise(resolve => setTimeout(resolve, 2500))
+    
+    const recommendGeekLink = (await pages[0].$('a[ka=menu-geek-recommend]'))
+    await recommendGeekLink.click()
+    await sleepWithRandomDelay(3000)
+  
+    const recommendIframeForControl = (await pages[0].frames()).find(it => it._name === 'recommendFrame')
+    const jobItemCount = (await recommendIframeForControl.$$(`.ui-dropmenu.job-selecter-wrap .ui-dropmenu-list .job-list .job-item`)).length
 
-    await sleepWithRandomDelay(2500)
-    pages[0].keyboard.press('ArrowRight')
+    for (let i = 0; i < jobItemCount; i++) {
+      const recommendIframe = (await pages[0].$('iframe[name=recommendFrame]'))
+      const recommendIframeForControl = (await pages[0].frames()).find(it => it._name === 'recommendFrame')
+
+      if (i > 0) {
+        const positionDropdown = await recommendIframeForControl.$(`.ui-dropmenu.job-selecter-wrap .ui-dropmenu-label`)
+        positionDropdown.click()
+        await sleepWithRandomDelay(500)
+
+        const jobItems = await recommendIframeForControl.$$(`.ui-dropmenu.job-selecter-wrap .ui-dropmenu-list .job-list .job-item`)
+        const currentJobItem = jobItems[i]
+        currentJobItem.click()
+        await sleepWithRandomDelay(3000)
+      }
+      const recommendIframeBBox = await recommendIframe.boundingBox()
+      await pages[0].mouse.move(
+        recommendIframeBBox.x + recommendIframeBBox.width / 2,
+        recommendIframeBBox.y + recommendIframeBBox.height / 2
+      )
+  
+      await pages[0].mouse.wheel({deltaY: 2000});
+      await sleepWithRandomDelay(1000)
+      await pages[0].mouse.wheel({deltaY: 2000});
+      await sleepWithRandomDelay(1000)
+      await pages[0].mouse.wheel({deltaY: 2000});
+      await sleepWithRandomDelay(1000)
+      await pages[0].mouse.wheel({deltaY: 2000});
+      await sleepWithRandomDelay(1000)
+      await pages[0].mouse.wheel({deltaY: 2000});
+      await sleepWithRandomDelay(1000)
+      await pages[0].mouse.wheel({deltaY: -10000});
+  
+      const firstGeekCardItemClickArea = await recommendIframeForControl.$(`ul.card-list li.card-item:first-child .card-inner.common-wrap`)
+      await sleepWithRandomDelay(1000)
+  
+      await firstGeekCardItemClickArea.click()
+      for (let i = 1; i <= 45; i++) {
+        await sleepWithRandomDelay(1000)
+  
+        const highlightItems = (await recommendIframeForControl.$$(`.resume-item.item-base .font-hightlight`)) ?? []
+        for(let j =0; j < highlightItems.length; j++) {
+          if (Math.random() > 0.5) {
+            continue
+          }
+          const it = highlightItems[j]
+          await sleepWithRandomDelay(200)
+          await it.click()
+        }
+  
+        await sleepWithRandomDelay(2500)
+        pages[0].keyboard.press('ArrowRight')
+      }
+      await sleepWithRandomDelay(1000)
+      // 关闭对话框
+      const resumeCloseButton = await recommendIframeForControl.$(`.dialog-recommend-resume .resume-custom-close`)
+      await resumeCloseButton.click()
+      // 收集一次har
+      debugger
+    }
+    // ;await browser.close()
+  } catch (err) {
+    console.error(err)
+    debugger
   }
-  await har.stop();
-
-  // ;await browser.close()
 })()
