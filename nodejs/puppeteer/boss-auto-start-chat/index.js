6fcde35526381e566eea63cc3eb43c2580c5da76
gogoend <gogoend@qq.com>
2023-12-30 18:25:39 +0800

    feat(node puppeteer boss-auto-start-chat): 加入自动开聊脚本（按照某求职期望下的推荐顺序，每次取队头开聊）；完善当日聊天次数用完提示判断 TODO: 加入限定公司进行开聊功能

---
+++ nodejs/puppeteer/boss-auto-start-chat/index.js
@@ -0,0 +1,113 @@
+const puppeteer = require('puppeteer');
+const cookies = require('./cookies')
+
+if (!cookies?.length) {
+  console.error('There is no cookies. you can save a copy with EditThisCookie extension.')
+  process.exit(1)
+}
+
+const asyncPoll = require('tiny-async-pool')
+
+const url = `https://www.zhipin.com/web/geek/job-recommend`
+const pages = []
+globalThis.pages = pages
+
+function sleep (t) {
+  return new Promise(resolve => {
+    setTimeout(resolve, t)
+  })
+}
+
+function sleepWithRandomDelay (base) {
+  return sleep(base + Math.random()*1000)
+}
+
+;(async () => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+      ignoreHTTPSErrors: true,
+      defaultViewport: {
+        width: 1440,
+        height: 900 - 140,
+      },
+      devtools: true
+    })
+  
+    const page = await browser.newPage()
+  
+    //设置cookie
+    for(let i = 0; i < cookies.length; i++){
+      await page.setCookie(cookies[i]);
+    }
+  
+    await page.goto(url, { timeout: 0 })
+    pages.push(page)
+  
+    await sleepWithRandomDelay(2500)
+    
+    const recommendJobLink = (await pages[0].$('[ka=header-job-recommend]'))
+    await recommendJobLink.click()
+
+    while (true) {
+      await sleepWithRandomDelay(4000)
+  
+      const expectJobList = (await pages[0].evaluate(`
+        document.querySelector('.job-recommend-search')?.__vue__?.expectList
+      `)
+      )
+      
+      const expectJobTabHandlers = await pages[0].$$('.job-recommend-main .recommend-search-expect .recommend-job-btn')
+      expectJobTabHandlers.shift()
+  
+      expectJobTabHandlers[0].click()
+      await page.waitForResponse(
+        response => {
+          if (
+            response.url().startsWith('https://www.zhipin.com/wapi/zpgeek/pc/recommend/job/list.json')
+          ) {
+            return true
+          }
+          return false
+        }
+      );
+      await sleepWithRandomDelay(2000)
+  
+      const jobData = await pages[0].evaluate('document.querySelector(".job-detail-box").__vue__.data')
+  
+      const startChatButtonInnerHTML = await pages[0].evaluate('document.querySelector(".job-detail-box .op-btn.op-btn-chat")?.innerHTML.trim()')
+      if (startChatButtonInnerHTML === '立即沟通') {
+        const startChatButtonProxy = await pages[0].$('.job-detail-box .op-btn.op-btn-chat')
+        await startChatButtonProxy.click()
+
+        const addFriendResponse = await page.waitForResponse(
+          response => {
+            if (
+              response.url().startsWith('https://www.zhipin.com/wapi/zpgeek/friend/add.json') && response.url().includes(`jobId=${jobData.jobInfo.encryptId}`)
+            ) {
+              return true
+            }
+            return false
+          }
+        );
+
+        const res = await addFriendResponse.json()
+
+        if (res.code === 1) {
+          if (res) {
+
+          }
+          console.log(res)
+          break
+        } 
+        await sleepWithRandomDelay(4000)
+        pages[0].goBack()
+      } else {
+      }
+    }
+
+    // ;await browser.close()
+  } catch (err) {
+    console.error(err)
+  }
+})()
